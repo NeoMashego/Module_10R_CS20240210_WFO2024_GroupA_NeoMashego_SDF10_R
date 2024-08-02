1329e7d7315260367a585372e3d7d7ea7ddcdab3
NeoMashego <mashegonoj@gmail.com>
2024-08-02 13:22:03 +0200

    added and removed comments in js file

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
-
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
+addButtonEl.addEventListener("click", function() {              //event listener listens to what useer does to produce output.
+    let inputValue = inputFieldEl.value                         //set variable = id.value(from id) to show in console.log
     
     console.log(inputValue)
 })
