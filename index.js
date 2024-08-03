da427f7293505436f58b559347ec52d928fd026c
NeoMashego <mashegonoj@gmail.com>
2024-08-03 12:04:19 +0200

    added import, const and push elements

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-/*import { initializeApp } from "./firebase-app.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app."
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://toyshoppingcart-default-rtdb.europe-west1.firebasedatabase.app/"
+    databaseURL: "https://playground-5b250-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 
 const app = initializeApp(appSettings)
-
-console.log(app)*/
-
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {              //event listener listens to what useer does to produce output.
     let inputValue = inputFieldEl.value                         //set variable = id.value(from id) to show in console.log
-    
+    push(shoppingListInDB, inputValue)
     console.log(inputValue)
 })
