e3123821acaed29c8f682209f439387d7ca051a5
NeoMashego <mashegonoj@gmail.com>
2024-08-03 10:12:55 +0200

    import firebase, console.log

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+
+const appSettings = {
+    databaseURL: "https://toyshoppingcart-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+
+console.log(app)
+
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
